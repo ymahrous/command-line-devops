230fa1d18254701f501e5cefa49de8df39f53ffc
Yousef Mahrous <yousefzentral@gmail.com>
2025-09-10 12:12:09 +0000

    init

---
+++ test_hello.py
@@ -0,0 +1,12 @@
+from click.testing import CliRunner
+from hello import search
+
+
+# search(path, ftype):
+
+
+def test_search():
+    runner = CliRunner()
+    result = runner.invoke(search, ["--path", ".", "--ftype", "py"])
+    assert result.exit_code == 0
+    assert ".py" in result.output
